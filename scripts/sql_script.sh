5a580ebde8aee818ed7d688ecd7404cc7a9fb9a0
Hexad-TechSupport <dishant.nankani@hexad.in>
2024-10-24 16:35:45 +0530

    updated with sql script

---
+++ scripts/sql_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Running SQL script..."
+# Define database connection parameters
+# DB_HOST="your-database-host"
+# DB_PORT="5433"
+# DB_NAME="your-database-name"
+# DB_USER="your-database-user"
+# DB_PASSWORD="your-database-password"
+# Run the SQL script using psql
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f /scripts/init.sql
+echo "SQL script executed."
