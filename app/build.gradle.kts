a9e8a49b49230125cc73a1f98cd7275d3b519af9
Dnayenshwar <dnyaneshwar.mule@hexad.de>
2024-10-24 22:54:19 +0530

    get trips, add trip and delete trip endpoint implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(20))
     }
 }
 
